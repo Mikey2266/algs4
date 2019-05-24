136a236f08a9c7e8d5b3d215c03097de1f36b7e3
Mikey2266 <601876177@qq.com>
2019-05-24 15:13:29 +0800

    before P138

---
+++ cpp/1_3_2_2-FixedCapacityStack.cpp
@@ -0,0 +1,93 @@
+/*泛型定容栈*/
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+template<class T>
+class ResizingArrayStack {
+private:
+    T* a;
+    int N;
+public:
+    ResizingArrayStack(int cap);
+    ~ResizingArrayStack();
+
+    bool isEmpty();
+    int size();
+    void push(T item);
+    T pop();
+};
+
+template<class T>
+ResizingArrayStack<T>::ResizingArrayStack(int cap) {
+    N = 0;
+    a = new T[cap];
+}
+
+template<class T>
+ResizingArrayStack<T>::~ResizingArrayStack() {
+    if (a) {
+        delete[] a;
+    }
+}
+
+template<class T>
+bool ResizingArrayStack<T>::isEmpty() {
+    return N == 0;
+}
+
+template<class T>
+int ResizingArrayStack<T>::size() {
+    return N;
+}
+
+template<class T>
+void ResizingArrayStack<T>::push(T item) {
+    a[N++] = item;
+}
+
+template<class T>
+T ResizingArrayStack<T>::pop() {
+    return a[--N];
+}
+
+int main() {
+    ResizingArrayStack<string> stack = ResizingArrayStack<string>(100);
+    // 手动输入测试
+    // string item;
+    // while(getline(cin, item)) {
+    //     if (item != "-") {
+    //         stack.push(atoi(item.c_str()));
+    //     }
+    //     else if (!stack.isEmpty()) {
+    //         cout << stack.pop() << endl;
+    //     }
+    //     else {
+    //         cout << "Stack is empty." << endl;
+    //     }
+    // }
+    // 文件输入测试
+    string path = "C:\\Users\\60187\\Desktop\\ItA\\algs4\\algs4-data\\";
+    string filename = "tobe.txt";
+    ifstream in(path + filename);  
+    string item;
+    if (in) {  
+        while (getline(in, item)) {   
+            if (item != "-") {
+                // stack.push(atoi(item.c_str()));
+                stack.push(item);
+            }
+            else if (!stack.isEmpty()) {
+                cout << stack.pop() << " ";
+            } 
+        }
+        cout << endl;
+    }  
+    else {  
+        cout <<"no such file" << endl;  
+    } 
+    return 0;
+}
---
+++ cpp/1_3_3_9-alg1_3-Queue+.cpp
@@ -0,0 +1,116 @@
+/*泛型先进先出队列, 未实现iterator函数*/
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+/*节点结构体*/
+template<class T>
+struct Node {
+    T item;
+    Node<T>* next;
+    Node(T x) : item(x), next(NULL) {}
+};
+
+template<class T>
+class Stack {
+private:
+    Node<T>* first;
+    Node<T>* last;
+    int N;
+public:
+    Stack();
+    ~Stack();
+
+    bool isEmpty();
+    int size();
+    void enqueue(T item);
+    T dequeue();
+};
+
+template<class T>
+Stack<T>::Stack() {
+    N = 0;
+    first = NULL;
+    last = NULL;
+}
+
+template<class T>
+Stack<T>::~Stack() {
+    delete first;
+    delete last;
+}
+
+template<class T>
+bool Stack<T>::isEmpty() {
+    return first == NULL;
+}
+
+template<class T>
+int Stack<T>::size() {
+    return N;
+}
+
+template<class T>
+void Stack<T>::enqueue(T item) {
+    Node<T>* oldLast = last;
+    last = new Node<T>(item);
+    if (isEmpty()) {
+        first = last;
+    }
+    else {
+        oldLast->next = last;
+    }
+    N++;
+}
+
+template<class T>
+T Stack<T>::dequeue() {
+    T item = first->item;
+    first = first->next;
+    if (isEmpty()) {
+        last = NULL;
+    }
+    N--;
+    return item;
+}
+
+int main() {
+    Stack<string> stack = Stack<string>();
+    // 手动输入测试
+    // string item;
+    // while(getline(cin, item)) {
+    //     if (item != "-") {
+    //         stack.push(atoi(item.c_str()));
+    //     }
+    //     else if (!stack.isEmpty()) {
+    //         cout << stack.pop() << endl;
+    //     }
+    //     else {
+    //         cout << "Stack is empty." << endl;
+    //     }
+    // }
+    // 文件输入测试
+    string path = "C:\\Users\\60187\\Desktop\\ItA\\algs4\\algs4-data\\";
+    string filename = "tobe.txt";
+    ifstream in(path + filename);  
+    string item;
+    if (in) {  
+        while (getline(in, item)) {   
+            if (item != "-") {
+                // stack.push(atoi(item.c_str()));
+                stack.enqueue(item);
+            }
+            else if (!stack.isEmpty()) {
+                cout << stack.dequeue() << " ";
+            } 
+        }
+        cout << endl;
+    }  
+    else {  
+        cout <<"no such file" << endl;  
+    } 
+    return 0;
+}
